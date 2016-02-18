11155e233c76350b799284af6b73e3fa7c9858de
edwardyu714 <eboxyz@gmail.com>
2016-02-18 11:20:06 -0800

    changing styling

--- public/js/main.js
+++ public/js/main.js
@@ -241,7 +241,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Are you ready to answer some weird qusetions?";
     log(message, {
       prepend: true
     });
